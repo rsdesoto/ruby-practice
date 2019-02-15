d838181d906b1ad96d064f9033feb40d76f7cf07
Ry <ry.e.desoto@gmail.com>
2019-02-14 21:39:07 -0600

    finished strings

---
+++ strings.rb
@@ -0,0 +1,7 @@
+puts "AA".length
+puts "AAAAA".length
+
+puts 3.odd? 
+puts 4.odd?
+puts 3.even?
+puts 4.even?
--- widgets.rb
+++ widgets.rb
@@ -1,8 +1,10 @@
 def ask(question)
-    print question
+    print question + " "
     gets
 end    
 
 puts "Welcome to the widget store!"
-answer = ask("How many widgets are you ordering? ")
-puts answer
+answer = ask("How many widgets are you ordering?")
+puts "You entered #{answer} widgets"
+p answer
+
