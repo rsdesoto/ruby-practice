c0735456521ed1555dba44dabf01119a94f0468b
Ry <ry.e.desoto@gmail.com>
2019-02-25 21:49:20 -0600

    finished binary array to number

--- round_2/codewars/binary_to_int/index.rb
+++ round_2/codewars/binary_to_int/index.rb
@@ -1,3 +1,15 @@
-def binary_array_to_number(arr)
-    # your code here
-end 
+class Codewars
+    def binary_array_to_number(arr)
+        # your code here
+        result = 0
+        arrlen = arr.length 
+        # puts arrlen
+        arr.each_with_index do |num,index|
+            # puts num
+            if num == 1
+                result += 2**(arrlen - index - 1)
+            end
+        end
+        return result
+    end 
+end
--- round_2/codewars/binary_to_int/index_spec.rb
+++ round_2/codewars/binary_to_int/index_spec.rb
@@ -1,1 +1,17 @@
-require_relative "./index_spec"
+require_relative "./index"
+
+describe Codewars do 
+    let(:test){ Codewars.new }
+
+    it "Works for 4-string arrays" do 
+        expect(test.binary_array_to_number([0,0,0,1])).to eq(1)
+        expect(test.binary_array_to_number([0,0,1,0])).to eq(2)
+        expect(test.binary_array_to_number([1,1,1,1])).to eq(15)
+        expect(test.binary_array_to_number([0,1,1,0])).to eq(6)
+    end
+
+    it "Works for 1-string arrays" do 
+        expect(test.binary_array_to_number([1])).to eq(1)
+        expect(test.binary_array_to_number([0])).to eq(0)
+    end
+end
