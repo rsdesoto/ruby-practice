6b4db0cb5c3543f53239fb31230d50a3b7036fc1
Ry <ry.e.desoto@gmail.com>
2019-03-02 23:23:19 -0600

    starting with blocks

--- round_2/blocks/start.rb
+++ round_2/blocks/start.rb
@@ -1,5 +1,18 @@
 
 
-3.times { |index|
-    puts "hi " + index.to_s
-}
+# 3.times { |index|
+#     puts "hi " + index.to_s
+# }
+
+# writing our own methods with blocks - 
+# block is an argument to the method
+
+def block_method 
+    yield
+    puts "This is the first line in block_method"
+    yield
+end
+
+block_method do 
+    puts "This statement is called from the block"
+end
