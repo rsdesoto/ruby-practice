bf6bcec8ffd287ebb4bc8a28d2fcaa7d72e527b3
Ry <ry.e.desoto@gmail.com>
2019-03-02 23:01:16 -0600

    spaceship operator

--- round_2/modules/trial.rb
+++ round_2/modules/trial.rb
@@ -1,10 +1,21 @@
 class Player 
+    include Comparable 
+
     attr_accessor :name, :score
 
-    
-    def initialize
+    def initialize(name,score)
         @name = name
         @score = score 
     end
+
+    def <=>(other_player)
+        score <=> other_player.score 
+    end
  
 end
+
+player1 = Player.new("ry",100)
+player2 = Player.new("nubs",2)
+
+puts "player1 > player2: %s" % (player1 > player2)
+puts "player1 < player2: %s" % (player1 < player2)
