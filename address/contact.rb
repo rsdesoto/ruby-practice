ffec7da41503a52b3ce64be4bb9c2128fff2a5c8
Ry <ry.e.desoto@gmail.com>
2019-03-10 20:12:28 -0500

    more methods

--- address/contact.rb
+++ address/contact.rb
@@ -18,6 +18,17 @@ class Contact
     def to_s
         "#{@first_name} #{@middle_name} #{@last_name}"
     end
+
+    def full_name 
+        full_name = first_name 
+        if @middle_name
+            full_name += " " 
+            full_name += middle_name
+        end
+        full_name += " "
+        full_name += last_name
+
+    end
 end
 
 ry = Contact.new
@@ -27,3 +38,10 @@ ry.last_name = "desoto"
 
 puts ry
 
+puts ry.full_name
+
+nubs = Contact.new
+nubs.first_name = "nubs"
+nubs.last_name = "potate"
+
+puts nubs.full_name
